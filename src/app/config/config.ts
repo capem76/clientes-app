92ddb1771fb645aca41e607c0240d05ae6346433
capem76 <capem76@hotmail.com>
2021-10-12 20:05:07 +0200

    cambio ip servicio tomcat a localhost

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,1 +1,3 @@
-export const URL_BACKEND = 'http://35.180.52.60/spring-boot-apirest'
+//ip localhost EC2 Amazon tomcat sever: 172.31.19.156:8080
+//ip externa tomcat server 35.180.52.60:8080
+export const URL_BACKEND = '172.31.19.156:8080/spring-boot-apirest'
