1d8b68934041e81d524f69d229eb33145b47c75d
capem76 <capem76@hotmail.com>
2021-10-19 23:41:45 +0200

    cambio en url backend para conectar desde apache

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,7 @@
 //ip localhost EC2 Amazon tomcat sever: 172.31.19.156:8080
 //ip externa tomcat server 35.180.52.60:8080
-export const URL_BACKEND = '172.31.19.156:8080/spring-boot-apirest'
+//localhost:8080/spring-boot-apirest
+//export const URL_BACKEND = 'http://localhost:8080';
+// SI SE EJECUTA SPRING BOOT EN JAR  http://www.c-pena.com:8080/
+// SI SE EJECTUA SPRING BOOT EN WAR http://www.c-pena.com:8080/spring-boot-apirest
+export const URL_BACKEND = 'http://www.c-pena.com:8080';
